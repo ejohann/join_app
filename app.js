aabdc5de64ecad8254270783093f5fbcea4708eb
Johanne <johanne_lewis@hotmail.com>
2019-06-28 13:46:28 +0100

    Add a joke route

--- app.js
+++ app.js
@@ -6,6 +6,11 @@ app.get("/", function(req, res){
 	res.send("You have reached our homepage!");
 });
 
+app.get("/joke", function(req, res){
+    var joke = "I know a guy with a wooden leg named smith.  What is the name of his other leg?";
+  	res.send(joke);
+});
+
 app.listen(8080, function(){
 	console.log("Server running on 8080!");
 });
