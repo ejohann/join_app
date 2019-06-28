5b3a3b5842bb43d8a583c713bb5084f8c303c82d
Johanne <johanne_lewis@hotmail.com>
2019-06-28 15:25:12 +0100

    Create markup for route to register a user view post request

--- app.js
+++ app.js
@@ -27,6 +27,12 @@ app.get("/", function(req, res){
    // connection.end();
 });
 
+
+app.post("/register", function(req, res){
+   console.log("Post request to register a user");
+});
+
+
 app.get("/joke", function(req, res){
     var joke = "I know a guy with a wooden leg named smith.  What is the name of his other leg?";
   	res.send(joke);
